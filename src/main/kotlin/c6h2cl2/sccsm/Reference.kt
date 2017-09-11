68748a6e59b7121edd2f27da36fccc90392d288a
Kamesuta <kamesuta@gmail.com>
2017-09-12 03:34:35 +0900

    IDEA調整 & Reference.kt

---
+++ src/main/kotlin/c6h2cl2/sccsm/Reference.kt
@@ -0,0 +1,15 @@
+package c6h2cl2.sccsm
+
+/**
+ * @author C6H2Cl2
+ * @author Kamesuta
+ */
+const val MODID = "sccsm"
+const val NAME = "SuperCompressedCobblestoneMod"
+const val VERSION = "\${version}"
+const val FORGE = "\${forgeversion}"
+const val MINECRAFT = "\${mcversion}"
+const val DEPENDENCIES = "required-after:forgelin"
+const val PROXY_SERVER = "c6h2cl2.sccsm.CommonProxy"
+const val PROXY_CLIENT = "c6h2cl2.sccsm.ClientProxy"
+const val GUI_FACTORY = "c6h2cl2.sccsm.gui.config.ConfigGuiFactory"
